5146be2c77a31033cb94a7ccc7d889bbee3ba27e
Ing. Frank De La Peña <ing.fpena@gmail.com>
2017-05-04 17:55:55 -0500

    Finalización de la clase singleton.

--- WebConnect/WebConnect.Core/AppCtx.cs
+++ WebConnect/WebConnect.Core/AppCtx.cs
@@ -1,6 +1,21 @@
-﻿namespace WebConnect.Core
+﻿using Spring.Context;
+using Spring.Context.Support;
+
+namespace WebConnect.Core
 {
     public class AppCtx
     {
+        private static AppCtx _instance;
+        private readonly IApplicationContext _application;
+
+        private static AppCtx Instance => _instance ?? (_instance = new AppCtx());
+        
+        private AppCtx() => _application = ContextRegistry.GetContext();
+
+        public static object Resolve(string name) => Instance._application.GetObject(name);
+
+        public static T Resolve<T>(string name) => (T)Resolve(name);
+        
+        public static T Resolve<T>() => (T)Resolve(typeof(T).Name);
     }
 }
