a35d80f013c8e1da0c749a3b976427e92ee4fada
Ing. Frank De La Peña <ing.fpena@gmail.com>
2017-05-08 16:44:40 -0500

    Creando pruebas para las dependencias

---
+++ WebConnect/WebConnect.Test/Reservas/DependenciaTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using WebConnect.Components.Reservas;
+
+namespace WebConnect.Test.Reservas
+{
+    [TestFixture]
+    public class DependenciaTest : AbstractTest<IDependenciaComponent>
+    {
+        [Test]
+        public void CreateDependencia()
+        {
+            
+        }
+    }
+}
